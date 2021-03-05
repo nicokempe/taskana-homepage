fca95f5089df964f808d14e7dcdd20f386e8aa45
Chi Nguyen <c.nguyen.prog@gmail.com>
2021-03-05 09:50:25 +0100

    add typescript, tailwindcss

---
+++ src/components/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">hero</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({})
+export default class Hero extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
